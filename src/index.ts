eae17c0a70859f4e6c7750c037786e1f57176dd1
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2020-11-01 00:38:23 +0100

    post graphql crud

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 // import { Post } from './entities/Post';
@@ -6,6 +7,7 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
 	const orm = await MikroORM.init(mikroConfig);
@@ -19,9 +21,10 @@ const main = async () => {
 
 	const apolloServer = new ApolloServer({
 		schema: await buildSchema({
+			resolvers: [HelloResolver, PostResolver],
 			validate: false,
-			resolvers: [HelloResolver],
 		}),
+		context: () => ({ em: orm.em }),
 	});
 	apolloServer.applyMiddleware({ app });
 
---
+++ src/resolvers/post.ts
@@ -0,0 +1,61 @@
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
+import { Post } from '../entities/Post';
+import { MyContext } from '../types';
+
+@Resolver()
+export class PostResolver {
+	@Query(() => [Post])
+	posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+		return em.find(Post, {});
+	}
+
+	@Query(() => Post, { nullable: true })
+	post(
+		@Arg('id', () => Int) id: number,
+		@Ctx() { em }: MyContext,
+	): Promise<Post | null> {
+		return em.findOne(Post, { id });
+	}
+
+	@Mutation(() => Post)
+	async createPost(
+		@Arg('title') title: string,
+		@Ctx() { em }: MyContext,
+	): Promise<Post> {
+		const post = em.create(Post, { title: title });
+		await em.persistAndFlush(post);
+		return post;
+	}
+
+	@Mutation(() => Post, { nullable: true })
+	async updatePost(
+		@Arg('id') id: number,
+		@Arg('title', { nullable: true }) title: string,
+		@Ctx() { em }: MyContext,
+	): Promise<Post | null> {
+		const post = await em.findOne(Post, { id });
+
+		if (!post) return null;
+
+		if (typeof title !== 'undefined') {
+			post.title = title;
+			await em.persistAndFlush(post);
+		}
+
+		return post;
+	}
+
+	@Mutation(() => Boolean)
+	async deletePost(
+		@Arg('id') id: number,
+		@Ctx() { em }: MyContext,
+	): Promise<boolean> {
+		try {
+			await em.nativeDelete(Post, { id });
+		} catch (error) {
+			return false;
+		}
+
+		return true;
+	}
+}
