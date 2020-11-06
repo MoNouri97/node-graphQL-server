108a0e5a4ffea0d22ff405397e9ffe66224c46d8
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2020-11-07 00:02:15 +0100

    email log more visible

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -29,5 +29,5 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
 	// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
 	// Preview only available when sending through an Ethereal account
-	console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+	console.log('💌 Preview URL: %s', nodemailer.getTestMessageUrl(info));
 };
